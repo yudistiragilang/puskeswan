12e7adb3e938996e8a1abdd7c92596cdfae8b722
Yudhistira Gilang <yudhistiragilang22@gmail.com>
2021-11-05 14:17:16 +0700

    base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/zero/';
+    public $baseURL = 'http://localhost/puskeswan/';
 
     /**
      * --------------------------------------------------------------------------
