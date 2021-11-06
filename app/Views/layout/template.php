3d42af98387ed4bfb1a84ca730e8c00f7e8aa613
Yudhistira Gilang <yudhistiragilang22@gmail.com>
2021-11-06 11:44:19 +0700

    remove cdn bootstarp 4 twitter

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -16,7 +16,6 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="<?= base_url('public/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" />
     <link href="<?= base_url('public/css/select2-bootstrap4.css'); ?>" rel="stylesheet">
 
